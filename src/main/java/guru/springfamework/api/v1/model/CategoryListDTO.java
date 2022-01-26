98729f6d53c8c89e6cfaa069add21b51c313673f
Connor Wheatley <cwheatley@tier2consulting.com>
2022-01-26 17:10:42 +0000

    Added category controller as well as a new data model

---
+++ src/main/java/guru/springfamework/api/v1/model/CategoryListDTO.java
@@ -0,0 +1,16 @@
+package guru.springfamework.api.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @Author: Connor Wheatley
+ * @Date: 26/01/2022 16:50
+ */
+@Data
+@AllArgsConstructor
+public class CategoryListDTO {
+    List<CategoryDTO> categories;
+}
